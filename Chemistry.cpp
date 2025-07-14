b45845bb4eafd422cd2ab82968f98a36bee778a9
ionion-2501 <ananyaanu3125@gmail.com>
2025-07-14 16:47:30 +0530

    Add files via upload

---
+++ Chemistry.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	long long t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+		long long n,k;
+		cin>>n>>k;
+		char s[n];
+		for(int j=0;j<n;j++)
+			cin>>s[j];
+		long freq[26]={0};
+		for(int j=0;j<n;j++)
+		{	
+			int k=(int)s[j];
+			++freq[k-97];
+		}
+		long f=0;
+		for(int j=0;j<26;j++)
+			f+=freq[j]%2;
+		if(f>k+1)
+			cout<<"NO\n";
+		else cout<<"YES\n";
+	}
+	return 0;
+}
